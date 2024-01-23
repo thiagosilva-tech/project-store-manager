bbd018b2ed3c6f91eff065d74f226892ed346fe5
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2024-01-23 11:24:49 -0300

    feat: testes mutation

--- backend/tests/unit/controllers/sales.controller.test.js
+++ backend/tests/unit/controllers/sales.controller.test.js
@@ -5,6 +5,9 @@ const sinonChai = require('sinon-chai');
 const { salesService } = require('../../../src/services');
 const { allSalesServiceResponseMock, allSalesMock } = require('../mocks/sales.mock');
 const { salesController } = require('../../../src/controllers');
+const validateSalesCreationFields = require('../../../src/middlewares/sales.middlewares');
+const validateItem = require('../../../src/utils/validateItem');
+const handleError = require('../../../src/utils/handleError');
 
 chai.use(sinonChai);
 
@@ -13,6 +16,9 @@ describe('Unit Tests - Sales Controller', function () {
     Sinon.restore();
   });
   describe('getAllSales', function () {
+    afterEach(function () {
+      Sinon.restore();
+    });
     it('Status deve ser chamado com 200 e json com lista de sales', async function () {
       /// Arrange
       Sinon.stub(salesService, 'getAllSales').resolves(allSalesServiceResponseMock);
@@ -29,7 +35,11 @@ describe('Unit Tests - Sales Controller', function () {
       expect(res.json).to.have.been.calledWith(allSalesMock);
     });
   });
+
   describe('getSalesById', function () {
+    afterEach(function () {
+      Sinon.restore();
+    });
     it('should call salesService.getSalesById and send response', async function () {
       // Arrange
       const saleId = '1';
@@ -63,6 +73,9 @@ describe('Unit Tests - Sales Controller', function () {
   });
    
   describe('create', function () {
+    afterEach(function () {
+      Sinon.restore();
+    });
     it('should call salesService.create and send response', async function () {
       // Arrange
       const saleBody = [];
@@ -92,6 +105,97 @@ describe('Unit Tests - Sales Controller', function () {
       // Assert
       expect(res.status).to.have.been.calledWith(404);
       expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
+    });
+  });
+
+  describe('validateSalesCreationFields', function () {
+    afterEach(function () {
+      Sinon.restore();
+    });
+    it('should call next() when the request body is valid', function () {
+      // Arrange
+      const mockRequest = {
+        body: [
+          { productId: 1, quantity: 5 },
+          { productId: 2, quantity: 3 },
+        ],
+      };
+      const mockResponse = {};
+      const mockNext = Sinon.stub();
+  
+      // Act
+      validateSalesCreationFields(mockRequest, mockResponse, mockNext);
+  
+      // Assert
+      expect(mockNext.calledOnce).to.be.equal(true);
+    });
+  
+    it('should call handleError() when the request body is invalid', function () {
+      // Arrange
+      const mockRequest = {
+        body: [
+          { productId: 'invalid', quantity: 5 },
+          { productId: 2, quantity: 0 },
+        ],
+      };
+      const mockResponse = {
+        status: Sinon.stub().returnsThis(),
+        json: Sinon.stub().returnsThis(),
+      };
+      const mockNext = Sinon.stub();
+      // const handleErrorStub = Sinon.stub(handleError);
+    
+      // Act
+      validateSalesCreationFields(mockRequest, mockResponse, mockNext);
+    
+      // Assert
+      // expect(handleErrorStub.called).to.be.equal(true);
+      expect(mockNext.called).to.be.equal(false);
+    });
+  });
+
+  describe('Utils Tests', function () {  
+    afterEach(function () {
+      Sinon.restore();
+    });
+    describe('handleError', function () {
+      it('should handle error with status 400', function () {
+        // Arrange
+        const res = { status: Sinon.stub().returnsThis(), json: Sinon.stub().returnsThis() };
+  
+        // Act
+        handleError(res, { details: [{ message: 'Validation Error' }] });
+  
+        // Assert
+        expect(res.status.calledWith(400)).to.be.equal(true);
+        expect(res.json.calledWith({ message: 'Validation Error' })).to.be.equal(true);
+      });
+  
+      // Add more test cases for different error scenarios
+    });
+  
+    describe('validateItem', function () {
+      it('should return null for valid item', function () {
+        // Arrange
+        const item = { productId: 1, quantity: 2 };
+  
+        // Act
+        const result = validateItem(item);
+  
+        // Assert
+        expect(result).to.be.equal(undefined);
+      });
+  
+      it('should return validation error for invalid item', function () {
+        // Arrange
+        const item = { productId: 'invalid', quantity: -1 };
+  
+        // Act
+        const result = validateItem(item);
+  
+        // Assert
+        expect(result.details[0].message).to.equal('Product not found');
+      });
     });
   });
 });
--- backend/tests/unit/services/sales.service.test.js
+++ backend/tests/unit/services/sales.service.test.js
@@ -3,6 +3,7 @@ const sinon = require('sinon');
 const { salesModel } = require('../../../src/models');
 const { salesService } = require('../../../src/services');
 const { allSalesFromModel, allSalesMock } = require('../mocks/sales.mock');
+const areAllProductsAvailable = require('../../../src/utils/areAllProductsAvailable');
 
 describe('Uni Tests - Sales Service', function () {
   it('Recuperando todos as vendas com sucesso', async function () {
@@ -10,5 +11,19 @@ describe('Uni Tests - Sales Service', function () {
 
     const responseService = await salesService.getAllSales();
     expect(responseService.data).to.deep.equal(allSalesMock);
+  });
+
+  describe('areAllProductsAvailable', function () {
+    it('should return true if all products are available', function () {
+      const body = [{ productId: 1 }, { productId: 2 }];
+      const products = [{ id: 1 }, { id: 2 }];
+      expect(areAllProductsAvailable(body, products)).to.be.equal(true);
+    });
+
+    it('should return false if a product is not available', function () {
+      const body = [{ productId: 1 }, { productId: 3 }];
+      const products = [{ id: 1 }, { id: 2 }];
+      expect(areAllProductsAvailable(body, products)).to.be.equal(false);
+    });
   });
 });
